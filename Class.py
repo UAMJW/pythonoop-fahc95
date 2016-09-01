9a203f34f06ecd576e12ab9b2d3d659924359ff7
Freddy Hernández <fahc95@gmail.com>
2016-08-31 19:41:14 -0600

    Declarar Metodos para clase

---
+++ Class.py
@@ -0,0 +1,22 @@
+class PC(Object):
+    def _init_(self, RAM, storage, marca, modelo, precio):
+        self._RAM = RAM
+        self._storage = storage
+        self._marca = marca
+        self._modelo = modelo
+        self._precio = mprecio
+
+    def SetDatos(self, RAM, storage, marca, modelo, precio):
+        self._RAM = RAM
+        self._storage = storage
+        self._marca = marca
+        self._modelo = modelo
+        self._precio = precio
+
+
+
+        
+    
+
+
+
